ca45f6db71aefebae6243786860fa4ce4e83849f
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2023-06-16 10:06:12 +0100

    initial commit

---
+++ src/main/java/io/undefvar/beanvalidationdemo/BeanValidationDemoApplication.java
@@ -0,0 +1,13 @@
+package io.undefvar.beanvalidationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanValidationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanValidationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/undefvar/beanvalidationdemo/BeanValidationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.undefvar.beanvalidationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanValidationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
